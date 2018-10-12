e73f1dced716d82babe0ce8bcfa19ea50289ee74
Nhu Nguyen <nhu313@gmail.com>
2018-10-12 15:18:18 -0400

    removed environment prod

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  host: "https://portal.zipcode.rocks",
-  apiUrl: "https://ersatz-aroura.herokuapp.com",
-  github_client: "",
-  google_client: ""
-};
