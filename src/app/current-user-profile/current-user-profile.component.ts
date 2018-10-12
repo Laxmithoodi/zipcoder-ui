a54cc6f13c6ca828cb8ffaf1b2318e0eb262bc95
Nhu Nguyen <nhu313@gmail.com>
2018-10-12 15:03:33 -0400

    oops more student profile fix

--- src/app/current-user-profile/current-user-profile.component.ts
+++ src/app/current-user-profile/current-user-profile.component.ts
@@ -5,6 +5,7 @@ import { Student } from './../student/student';
 import { StudentService } from './../student/student.service';
 import { AuthService } from './../auth/auth.service';
 import { Lab } from './../lab/lab';
+import { Assessment } from './../assessment/assessment';
 
 import * as M from "materialize-css/dist/js/materialize";
 
@@ -18,6 +19,7 @@ export class CurrentUserProfileComponent implements OnInit {
   student: Student = new Student();
   id: number;
   labs: Lab[] = [];
+  assessments: Assessment[] = [];
 
   constructor(private auth: AuthService, private service: StudentService) {
     this.id = auth.getStudentId();
@@ -26,6 +28,7 @@ export class CurrentUserProfileComponent implements OnInit {
   ngOnInit() {
     this.service.get(this.id).subscribe(data => this.student = data);
     this.service.getAssignments(this.id).subscribe(data => this.labs = data);
+    this.service.getAssessments(this.id).subscribe(data => this.assessments = data);
   }
 
   getNotCompleted(labs) {
