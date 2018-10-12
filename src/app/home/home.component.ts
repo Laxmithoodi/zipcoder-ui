4f86aa212c4d2cd0191d2f59b96d87279c1e36ba
Nhu Nguyen <nhu313@gmail.com>
2018-10-12 17:31:59 -0400

    redirect when user is authenticated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { v4 as uuid } from 'uuid';
 import { environment } from '../../environments/environment';
+import { AuthService } from './../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +15,17 @@ export class HomeComponent implements OnInit {
   google_client: string = environment.google_client;
   github_client: string = environment.github_client;
 
-  constructor() {}
+  constructor(private auth: AuthService,
+              private router: Router) {}
 
   ngOnInit() {
+    if (this.auth.isAuthenticated()) {
+      if (this.auth.isStudent()) {
+        this.router.navigate(['/profile']);
+      } else {
+        this.router.navigate(['/students'])
+      }
+    }
   }
 
   getRandomString(){
