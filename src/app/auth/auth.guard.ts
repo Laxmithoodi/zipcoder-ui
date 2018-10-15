fa56063cada7e43c1d3fc83c5106503d3a349535
Nhu Nguyen <nhu313@gmail.com>
2018-10-15 14:20:06 -0400

    added staff check

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   canActivate(
+    auth: AuthService,
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      //do auth check here
-    return true;
+
+    return this.checkLogin();
+  }
+
+  checkLogin(): boolean {
+    if (this.authService.isAuthenticated() && this.authService.isStaff()) {
+      return true;
+    }
+
+    // Navigate to the login page with extras
+    this.router.navigate(['/']);
+    return false;
   }
 }
